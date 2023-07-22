8baa573095bd05f43205a7894a36e2f289d4fb1c
andresfeelip29 <andreefeelip29@gmail.com>
2023-07-22 17:33:19 -0500

    add eureka server base

---
+++ src/test/java/com/co/app/eureka/SpringbootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
